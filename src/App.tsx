c17a084dd1e03c736bd2b7bce39acd467a506a9f
anzorikt1506 <alirush2017@yandex.ru>
2022-10-03 17:31:35 +0300

    03.10.2022 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,36 +5,38 @@ import { testSlice } from './store/reducers/testSlice';
 
 import Content from './Component/Content/Content';
 import Sitebar from './Component/Sitebar/Sitebar';
+import LoginC from './Component/Login/LoginC';
 
 
 
 	
-function notifyMe() {
-  if (!("Notification" in window)) {
-    alert("Браузер не поддерживает уведомления");
-  } else if (Notification.permission === "granted") {
-    const notification = setTimeout(()=>{new Notification("Hi there!", {
-			tag : "ache-mail",
-			body : "Пора сделать паузу и отдохнуть",
-			icon : "http://kinolife.su/content/img/kino-img/15693665524683.jpg"
-		})}, 2000)  ;
-  } else if (Notification.permission !== "denied") {
-    Notification.requestPermission().then((permission) => {
-      if (permission === "granted") {
-        const notification = setTimeout(()=>{new Notification("Hi there!")}, 2000)  ;
-      }
-    });
-  }
-}
+// function notifyMe() {
+//   if (!("Notification" in window)) {
+//     alert("Браузер не поддерживает уведомления");
+//   } else if (Notification.permission === "granted") {
+//     const notification = setTimeout(()=>{new Notification("Hi there!", {
+// 			tag : "ache-mail",
+// 			body : "Пора сделать паузу и отдохнуть",
+// 			icon : "http://kinolife.su/content/img/kino-img/15693665524683.jpg"
+// 		})}, 2000)  ;
+//   } else if (Notification.permission !== "denied") {
+//     Notification.requestPermission().then((permission) => {
+//       if (permission === "granted") {
+//         const notification = setTimeout(()=>{new Notification("Hi there!")}, 2000)  ;
+//       }
+//     });
+//   }
+// }
 
 function App() {
 console.log()
   return (
     <>
     <div id="wrapper">
- <input type="button" onClick={notifyMe} value="Notification"/> 
-    <Sitebar/>
-    <Content/>
+      <LoginC/>
+ {/* <input type="button" onClick={notifyMe} value="Notification"/>  */}
+    {/* <Sitebar/>
+    <Content/> */}
     </div>
     </>
   );
---
+++ src/Component/Login/LoginC.tsx
@@ -0,0 +1,17 @@
+import { eventNames } from "process";
+import React from "react";
+import Login from "./Login";
+
+// interface ObjectF {
+
+// }
+const LoginC: React.FC = ({
+
+}) => {
+
+  return (
+   <Login/>
+  );
+};
+
+export default  React.memo(LoginC);
