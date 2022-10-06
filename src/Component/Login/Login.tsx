6eca26f58fc63d752b89a1b7b8d0d82a1aa9e438
anzorikt1506 <alirush2017@yandex.ru>
2022-10-06 17:11:40 +0300

    03.10.2022 3

--- src/Component/Login/Login.tsx
+++ src/Component/Login/Login.tsx
@@ -1,11 +1,17 @@
 import { eventNames } from "process";
 import React from "react";
 
-// interface ObjectF {
-
-// }
-const Login: React.FC = ({
-
+interface ObjectF {
+  login:string,
+  password:string,
+  changeLogin:(e:any)=>void,
+  changePassword:(e:any)=>void
+}
+const Login: React.FC<ObjectF> = ({
+  login,
+  password,
+  changeLogin,
+  changePassword
 }) => {
 
   return (
@@ -15,53 +21,37 @@ const Login: React.FC = ({
 
 <div className="row justify-content-center">
 
-    <div className="col-xl-10 col-lg-12 col-md-9">
+    <div className="col-xl-4 col-lg-12 col-md-9">
 
         <div className="card o-hidden border-0 shadow-lg my-5">
             <div className="card-body p-0">
             
                 <div className="row">
-                    <div className="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                    <div className="col-lg-6">
+
+                    <div className="col-lg-12">
                         <div className="p-5">
                             <div className="text-center">
-                                <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                <h1 className="h4 text-gray-900 mb-4">Авторизуйтесь</h1>
                             </div>
                             <form className="user">
                                 <div className="form-group">
-                                    <input type="email" className="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                        placeholder="Enter Email Address..."/>
+                                    <input type="text" onChange={changeLogin} className="form-control form-control-user"
+                                        id="exampleInputEmail" value={login} aria-describedby="emailHelp"
+                                        placeholder="Введите логин"/>
                                 </div>
                                 <div className="form-group">
-                                    <input type="password" className="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password"/>
+                                    <input onChange={changePassword} type="password" className="form-control form-control-user"
+                                        id="exampleInputPassword" value={password} placeholder="Введите пароль"/>
                                 </div>
-                                <div className="form-group">
-                                    <div className="custom-control custom-checkbox small">
-                                        <input type="checkbox" className="custom-control-input" id="customCheck"/>
-                                        {/* <label className="custom-control-label" for="customCheck">Remember
-                                            Me</label> */}
-                                    </div>
-                                </div>
+
                                 <a href="index.html" className="btn btn-primary btn-user btn-block">
-                                    Login
+                                    Отправить
                                 </a>
-                                <hr/>
-                                <a href="index.html" className="btn btn-google btn-user btn-block">
-                                    <i className="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="index.html" className="btn btn-facebook btn-user btn-block">
-                                    <i className="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a>
+                                
+
                             </form>
-                            <hr/>
-                            <div className="text-center">
-                                <a className="small" href="forgot-password.html">Forgot Password?</a>
-                            </div>
-                            <div className="text-center">
-                                <a className="small" href="register.html">Create an Account!</a>
-                            </div>
+                            
+
                         </div>
                     </div>
                 </div>
--- src/Component/Login/LoginC.tsx
+++ src/Component/Login/LoginC.tsx
@@ -1,6 +1,8 @@
 import { eventNames } from "process";
 import React from "react";
+import { useState } from "react";
 import Login from "./Login";
+
 
 // interface ObjectF {
 
@@ -9,8 +11,17 @@ const LoginC: React.FC = ({
 
 }) => {
 
+    const [login, loginF] = useState<string>('');
+    const [password, passwordF] = useState<string>('');
+    const changeLogin = (e:any) => loginF(e.target.value)
+    const changePassword = (e:any) => passwordF(e.target.value)
   return (
-   <Login/>
+   <Login
+   login={login}
+   password={password}
+   changeLogin={changeLogin}
+   changePassword={changePassword}
+   />
   );
 };
 
